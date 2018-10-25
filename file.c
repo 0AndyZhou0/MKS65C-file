bd719f269bd86d1d4af748d8654b89f47d56588e
Andy Zhou <azhou4@stuy.edu>
2018-10-25 15:24:08 -0400

    you have to redefine file to read from the start

--- file.c
+++ file.c
@@ -5,22 +5,30 @@
 #include <unistd.h>
 
 int main(){
-  int thing = open("./test.txt", O_RDWR | O_APPEND);
+  int thing = open("./test.txt", O_RDWR/* | O_APPEND*/);
+  if(thing < 0){
+    printf("ERROR\n");
+    _exit(0);
+  }
 
   if(write(thing, "I don't know what I'm doing\n", 28) < 0){
     printf("ERROR\n");
     _exit(0);
   }
 
+  thing = open("./test.txt", O_RDWR);
   char string[100];
-  if(read(thing, string, 27) < 0){
+  if(read(thing, string, 28) < 0){
+    printf("ERROR\n");
+    _exit(0);
+  }else{
+    printf("%s\n", string);
+  }
+
+  if(close(thing) < 0){
     printf("ERROR\n");
     _exit(0);
   }
-
-  printf("%s\n", string);
-  if(close(thing) < 0){
-    printf("closed\n");
-  }
+  printf("closed\n");
   return 0;
 }
